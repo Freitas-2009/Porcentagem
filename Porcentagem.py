092c56437c7dbd417888840bd29e1957b2e42a91
Freitas-2009 <luisfernando.cfreitas@gmail.com>
2025-09-05 13:20:10 -0300

    Add files via upload

---
+++ Porcentagem.py
@@ -0,0 +1,5 @@
+Valor = float(input("digite o valor"))
+porcentagem = int(input("digite a porcentagem "))
+
+Resultado = Valor * (porcentagem / 100)
+print (f"O resultado é: {porcentagem}% de {Valor} = {Resultado}")
